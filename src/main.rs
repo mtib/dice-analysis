4180c065f147b276dd5c2752feaec429e9708992
Markus Becker <mtb@tibyte.net>
2016-08-05 18:15:58 +0200

    analysis beginning
    I am taking a break now :D

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ const SELECT: u8 = 3;
 fn main() {
     match env::args().nth(1) {
         Some(s) => match s.chars().nth(0) {
-            Some('a') => analyze(),
+            Some('a') => {analyze(); ()},
             Some('b') => {brute(); ()},
             Some('c') => compare(),
             _ => println!("unknown option")
@@ -25,8 +25,58 @@ fn main() {
     };
 }
 
-fn analyze() {
+fn analyze() -> Vec<u32>{
+    let fact = |x| {
+        (1..x+1).fold(1, |a, b| a * b) as u32
+    };
     println!("analyzing");
+    let vkw = (SIDES as u32).pow(DICE as u32);
+    let mut prob = vec![0u32;((SIDES * SELECT) - SELECT + 1) as usize];
+    for res in SELECT..SELECT*SIDES+1 {
+        // println!("{0:>2} -> ???/{1} | {0:>2}! = 10^{2:.2}", res, vkw, (fact(res as u64) as f64).log10());
+        let mut pos = Vec::<Vec<u8>>::new();
+        // TODO generating all possible vecs for result res
+        // They have to be unique
+        {
+            let mut scount = DICE;
+            let mut i = 0usize;
+            let mut startvec = vec![1; DICE as usize];
+            loop {
+                if res >= scount + SIDES - 1{
+                    startvec[i] = SIDES;
+                    i += 1;
+                    scount += SIDES - 1;
+                } else {
+                    startvec[i] = res + SELECT - scount - 1;
+                    pos.push(startvec);
+                    println!("   /-> {:?}", pos[pos.len()-1]);
+                    break;
+                }
+            }
+        }
+        loop {
+            // pos.push(vec![3,1,2,3]);
+            // permutatie pos[0] into every other possible role
+            // with the same sum of the SELECT biggest elements
+            break;
+        }
+        // ---
+        let mut variations = 0;
+        for p in pos { // consumes
+            let mut sames = [0u8; SIDES as usize];
+            for val in p { // consumes
+                sames[val as usize -1] += 1;
+            }
+            let mut var = fact(DICE);
+            for occ in &sames {
+                var /= fact(*occ);
+            }
+            variations += var;
+        }
+        println!("{:>3}: {:>5}/{} = {:>5.2}%", res, variations, vkw, variations as f64 / vkw as f64);
+        prob[(res - SELECT) as usize] = variations;
+    }
+    prob
 }
 
 fn brute() -> Vec<u32> {
