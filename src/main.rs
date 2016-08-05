d7674c5a0809d6b90ccebf4be2665331eef67617
Markus Becker <mtb@tibyte.net>
2016-08-05 16:18:47 +0200

    added generalisation

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ const HELP: &'static str = "Options:
 [c]ompare";
 const RUNS: u64 = 10_000_000;
 const DICE: u8 = 4;
+const SIDES: u8 = 6;
+const SELECT: u8 = 3;
 
 fn main() {
     match env::args().nth(1) {
@@ -32,7 +34,7 @@ fn brute() -> Vec<u32> {
     let mut rolls = Vec::<u16>::new();
     let perc = RUNS / 100;
     for i in 1..RUNS+1 {
-        rolls.push(roll(DICE).iter().take(3).fold(0,|sum, x| sum + x) as u16);
+        rolls.push(roll(DICE).iter().take(SELECT as usize).fold(0,|sum, x| sum + x) as u16);
         if i % perc == 0 {
             print!("{:>3}% ", i*100/RUNS);
             if i % (perc*10) == 0 {
@@ -41,12 +43,12 @@ fn brute() -> Vec<u32> {
             io::stdout().flush().unwrap();
         }
     }
-    let mut data = vec![0u32;16];
+    let mut data = vec![0u32; ((SIDES * SELECT) - SELECT + 1) as usize];
     for n in rolls { //consumes
-        data[(n-3) as usize] += 1;
+        data[(n as u8-SELECT) as usize] += 1;
     }
     println!("results");
-    let mut rn = 3;
+    let mut rn = SELECT;
     for r in &data {
         println!("{:>2}: {:>10}/{} = {:>5.2}%", rn, r, RUNS, *r as f64 / RUNS as f64 * 100f64);
         rn += 1;
@@ -61,7 +63,7 @@ fn compare() {
 fn roll(len: u8) -> Vec<u8> {
     let mut arr = Vec::<u8>::new();
     for _ in 0..len {
-        arr.push(rand::random::<u8>()%6+1);
+        arr.push(rand::random::<u8>()%SIDES+1);
     }
     arr.sort_by(|a, b| b.cmp(a));
     arr
