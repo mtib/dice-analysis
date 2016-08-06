bfd548019d23046b03f4b813395a9fde546a6986
Markus Becker <mtb@tibyte.net>
2016-08-06 22:18:48 +0200

    rustfmt
    just some fmt

--- src/main.rs
+++ src/main.rs
@@ -18,14 +18,25 @@ const SELECT: usize = 3;
 
 fn main() {
     match env::args().nth(1) {
-        Some(s) => match s.chars().nth(0) {
-            Some('a') => {analyze(); ()},
-            Some('b') => {brute(); ()},
-            Some('c') => compare(),
-            Some('l') => {list_analyze(); ()},
-            _ => println!("unknown option")
-        },
-        None => println!("{}", HELP)
+        Some(s) => {
+            match s.chars().nth(0) {
+                Some('a') => {
+                    analyze();
+                    ()
+                }
+                Some('b') => {
+                    brute();
+                    ()
+                }
+                Some('c') => compare(),
+                Some('l') => {
+                    list_analyze();
+                    ()
+                }
+                _ => println!("unknown option"),
+            }
+        }
+        None => println!("{}", HELP),
     };
 }
 
@@ -33,8 +44,7 @@ fn analyze() -> Vec<usize> {
     println!("analyzing");
     let vkw = SIDES.pow(DICE as u32);
     let mut prob = vec![0usize;SIDES * SELECT - SELECT + 1];
-    for res in SELECT..SELECT*SIDES+1 {
-        // println!("{0:>2} -> ???/{1} | {0:>2}! = 10^{2:.2}", res, vkw, (fact(res as u64) as f64).log10());
+    for res in SELECT..SELECT * SIDES + 1 {
         let mut pos = Vec::<Vec<usize>>::new();
         // TODO generating all possible vecs for result res
         // They have to be unique
@@ -45,7 +55,7 @@ fn analyze() -> Vec<usize> {
             let mut i = 0usize;
             let mut startvec = vec![1; DICE];
             loop {
-                if res >= scount + SIDES - 1{
+                if res >= scount + SIDES - 1 {
                     startvec[i] = SIDES;
                     i += 1;
                     scount += SIDES - 1;
@@ -53,7 +63,7 @@ fn analyze() -> Vec<usize> {
                     // FIXME this brakes on SELECT >= DICE
                     startvec[i] = res + SELECT - scount - 1;
                     pos.push(startvec);
-                    println!("   /-> {:?}", pos[pos.len()-1]);
+                    println!("   /-> {:?}", pos[pos.len() - 1]);
                     break;
                 }
             }
@@ -77,10 +87,15 @@ fn analyze() -> Vec<usize> {
         }
         // ---
         let mut variations = 0;
-        for p in pos { // consumes
+        for p in pos {
+            // consumes
             variations += permutations(p);
         }
-        println!("{:>3}: {:>5}/{} = {:>5.2}%", res, variations, vkw, variations as f64 / vkw as f64);
+        println!("{:>3}: {:>5}/{} = {:>5.2}%",
+                 res,
+                 variations,
+                 vkw,
+                 variations as f64 / vkw as f64);
         prob[res - SELECT] = variations;
     }
     prob
@@ -90,24 +105,29 @@ fn brute() -> Vec<usize> {
     println!("brute forcing it up ({})", RUNS);
     let mut rolls = Vec::<usize>::new();
     let perc = RUNS / 100;
-    for i in 1..RUNS+1 {
-        rolls.push(roll::<usize>(DICE).iter().take(SELECT).fold(0,|sum, x| sum + x));
+    for i in 1..RUNS + 1 {
+        rolls.push(roll::<usize>(DICE).iter().take(SELECT).fold(0, |sum, x| sum + x));
         if i % perc == 0 {
-            print!("{:>3}% ", i*100/RUNS);
-            if i % (perc*10) == 0 {
+            print!("{:>3}% ", i * 100 / RUNS);
+            if i % (perc * 10) == 0 {
                 print!("\n");
             }
             io::stdout().flush().unwrap();
         }
     }
     let mut data = vec![0usize; (SIDES * SELECT) - SELECT + 1];
-    for n in rolls { //consumes
+    for n in rolls {
+        // consumes
         data[n] += 1;
     }
     println!("results");
     let mut rn = SELECT;
     for r in &data {
-        println!("{:>2}: {:>10}/{} = {:>5.2}%", rn, r, RUNS, *r as f64 / RUNS as f64 * 100f64);
+        println!("{:>2}: {:>10}/{} = {:>5.2}%",
+                 rn,
+                 r,
+                 RUNS,
+                 *r as f64 / RUNS as f64 * 100f64);
         rn += 1;
     }
     data
@@ -120,24 +140,28 @@ fn compare() {
 fn append_possible(mut head: [usize; DICE], index: usize, result: &mut [usize]) {
     if index < DICE {
         for nd in 0..SIDES {
-            head[index] = nd+1;
-            append_possible(head, index+1, result);
+            head[index] = nd + 1;
+            append_possible(head, index + 1, result);
         }
     } else {
         head.sort_by(|a, b| b.cmp(a));
         let r = head.iter().take(SELECT).fold(0, |sum, x| sum + x);
-        result[r-SELECT] += 1;
+        result[r - SELECT] += 1;
     }
 }
 
 fn list_analyze() {
     println!("analyzing list of all possible rolls (cheat)");
     let vkw = SIDES.pow(DICE as u32) as f64;
-    let mut result = [0usize; SIDES*SELECT-SELECT+1];
+    let mut result = [0usize; SIDES * SELECT - SELECT + 1];
     let head = [1usize; DICE];
     append_possible(head, 0, &mut result);
     for x in 0..result.len() {
-        println!("{:>3}: {:>5}/{:.0} = {:>5.2}%", x+SELECT, result[x], vkw, result[x] as f64 / vkw * 100f64);
+        println!("{:>3}: {:>5}/{:.0} = {:>5.2}%",
+                 x + SELECT,
+                 result[x],
+                 vkw,
+                 result[x] as f64 / vkw * 100f64);
     }
 }
 
@@ -145,20 +169,19 @@ fn roll<T: rand::Rand + Num + NumCast + Ord + Copy>(len: usize) -> Vec<T> {
     let mut arr = Vec::<T>::new();
     let upper: T = NumCast::from(SIDES).unwrap();
     for _ in 0..len {
-        arr.push(rand::random::<T>()%upper);
+        arr.push(rand::random::<T>() % upper);
     }
     arr.sort_by(|a, b| b.cmp(a));
     arr
 }
 
 fn permutations<T: Ord + Num + NumCast>(arr: Vec<T>) -> usize {
-    let fact = |x| {
-        (1..x+1).fold(1, |a, b| a * b)
-    };
+    let fact = |x| (1..x + 1).fold(1, |a, b| a * b);
     let mut sames = [0; SIDES];
-    for val in arr { // consumes
+    for val in arr {
+        // consumes
         let i: usize = NumCast::from(val).unwrap();
-        sames[i-1] += 1;
+        sames[i - 1] += 1;
     }
     let mut var = fact(DICE);
     for occ in &sames {
